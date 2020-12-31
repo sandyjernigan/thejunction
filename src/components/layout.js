0022c4f20ff6ee15c7fceffc68f6f9077cd6a489
Sandy <sandy.jernigan@gmail.com>
2020-12-30 22:32:40 -0500

    Add a site title

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import React from "react"
 export default function Layout({ children }) {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <h3>MySweetSite</h3>
       {children}
     </div>
   )

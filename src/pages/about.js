d3abc94de6c3a89764c11b0971c5a4cd721bad26
Sandy <sandy.jernigan@gmail.com>
2020-12-30 22:29:01 -0500

    Creating layout components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from "react"
-import Header from "../components/header"
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
-      <Header headerText="About Gatsby" />
-      <p>Such wow. Very React.</p>
+    <div>
+      <h1>About me</h1>
+      <p>
+        I’m good enough, I’m smart enough, and gosh darn it, people like me!
+      </p>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
 
 export default function Contact() {
   return (
-    <div style={{ color: `teal` }}>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Header headerText="Contact" />
-      <p>Send us a message!</p>
+    <div>
+      <h1>I'd love to talk! Email me at the address below</h1>
+      <p>
+        <a href="mailto:me@example.com">me@example.com</a>
+      </p>
     </div>
   )
 }

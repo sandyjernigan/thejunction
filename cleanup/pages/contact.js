e3276bd1372f68d9ac49e451bf97e8221209dfe1
Sandy <sandy.jernigan@gmail.com>
2021-01-01 03:11:40 -0500

    Cleaning up from Tutorial

---
+++ cleanup/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Contact() {
+  return (
+    <Layout>
+    <div>
+      <h1>I'd love to talk! Email me at the address below</h1>
+      <p>
+        <a href="mailto:me@example.com">me@example.com</a>
+      </p>
+    </div>
+    </Layout>
+  )
+}
